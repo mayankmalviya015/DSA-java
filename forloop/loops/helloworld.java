bfd96691fd9ff83c6ff705e88d974b58b5e9e25d
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-02 23:13:45 +0530

    changes

--- forloop/loops/helloworld.java
+++ forloop/loops/helloworld.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class helloworld {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
 
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            System.out.println("Hello World");
+            for (int i = 1; i <= n; i++) {
+                System.out.println("Hello World");
+            }
         }
 
         

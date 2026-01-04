7b78ce0d72a0a6f19fc62a770d3da18640e26686
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-04 11:05:42 +0530

    changes

--- loops/Ap2.java
+++ loops/Ap2.java
@@ -1,6 +1,7 @@
 
 import java.util.Scanner;
 public class Ap2 {
+
 public static void main(String[] args) {
     // problem : display this airthmatic progession - 4 , 7 , 10 , 13 , 16 ..... upto 'n' number of term 
     Scanner sc = new Scanner(System.in);

b50319460297370ca91a124c2d90e9b70fb700b6
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-05 16:51:31 +0530

    changes

--- arrays/basicofarray.java
+++ arrays/basicofarray.java
@@ -11,9 +11,7 @@ public static void main(String[] args) {
         arr[i] = sc.nextInt();
     }
     for (int i = 0; i < n; i++) {
-        System.out.print(arr[i]+ "");
+        System.out.print(arr[i]+ "  ");
     }
-
-
 }
 }

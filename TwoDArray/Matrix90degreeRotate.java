9508921590487a4d17eda6fec7145f309b0a320c
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-17 18:09:09 +0530

    add new file

--- TwoDArray/Matrix90degreeRotate.java
+++ TwoDArray/Matrix90degreeRotate.java
@@ -1,4 +1,4 @@
-public class TransposeUsingonearray {
+public class Matrix90degreeRotate {
     public static void print(int[][] arr){
               for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -13,15 +13,29 @@ public class TransposeUsingonearray {
         int[][] arr ={{1 ,2 ,3},{4 ,5 ,6},{7 , 8 , 9}};
         print(arr);
         int m = arr.length; // in square matrix m = n
+        //Transposing.........
         for(int i=0 ;i< m; i++){
             for(int j =0 ; j<i ; j++){
-                //swap......
+                
                 int temp =arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
+
             }
         }
-
+        print(arr);
+        //rotate -> reverse each rows.....
+        for(int i =0 ; i< m ; i++){
+        int a = 0;
+        int b = m-1;
+        while(a<b){
+                int x =arr[i][a];
+                arr[i][a] = arr[i][b];
+                arr[i][b] = x;
+                a++;
+                b--;
+        }
+    }
          print(arr);
 
     }

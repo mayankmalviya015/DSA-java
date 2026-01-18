d9fd38d969651c687dfcba3b6d1e6fbfc66f5055
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-18 09:08:50 +0530

    add new file

---
+++ TwoDArray/WavePrinting.java
@@ -0,0 +1,21 @@
+public class WavePrinting {
+      public static void main(String[] args) {
+        int[][] arr ={{1 ,2 ,3},{4 ,5 ,6},{7 , 8 , 9},{3, 2 ,1 }};
+        int m = arr.length;
+        int n = arr[0].length;
+        for(int i =0; i<m ; i++){
+            if(i%2==0){
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j]+ " ");
+                    
+                }
+            }
+            else{
+                for (int j = n-1; j >=0; j--) {
+                    System.out.print(arr[i][j]+ " ");
+            }
+        }
+
+    }
+}
+}

f6afa93ae375bf22b6f1dfb93e43e818977ee650
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-21 23:27:58 +0530

    add pascal question

--- TwoDArray/MatrixMultiplication.java
+++ TwoDArray/MatrixMultiplication.java
@@ -8,7 +8,6 @@ public class MatrixMultiplication {
         }
         System.out.println();
     }
-
     public static void main(String[] args) {
         int[][] a = {{1 ,2 ,1}, { 2, 1, 2}};
         int[][] b = {{1 ,0 ,1 ,2 } ,{2 , 1, 0 , 0},{ 0 ,3 ,1 ,1}};
@@ -23,7 +22,7 @@ public class MatrixMultiplication {
                         c[i][j] += a[i][k] * b[k][j];
                     }
                 }
-            }  print(a);
+            } print(a);
         print(b);
         print(c);
         

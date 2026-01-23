88a046ba1619a88eec7da3c9a991800969ba0102
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-23 23:20:20 +0530

    add new file

---
+++ strings/stringsbasic.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class stringsbasic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name ->");
+        String str= sc.nextLine();
+        System.err.print("hi ");
+        System.out.println(str);
+    }
+}

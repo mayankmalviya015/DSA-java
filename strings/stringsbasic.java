927a1ca72b0d7b89d6a1fabebe1ba68562bb029d
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-01-24 22:06:08 +0530

    add new String

--- strings/stringsbasic.java
+++ strings/stringsbasic.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class stringsbasic {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter your name ->");
-        String str= sc.nextLine();
-        System.err.print("hi ");
-        System.out.println(str);
+        System.out.println("enter your name");
+         String x = sc.nextLine();
+        String y ="hii";
+        System.out.print(y+" ");
+        System.out.println(x);
     }
 }

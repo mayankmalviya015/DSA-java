479ca8066cbaca389c96147d4373d83f3998872e
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-02-15 00:54:10 +0530

    add bubble sort technique

--- Sorting/BubbleSort2.java
+++ Sorting/BubbleSort2.java
@@ -1,4 +1,4 @@
-public class BubbleSort {
+public class BubbleSort2 {
     public static void print(int[] arr){
         for(int ele : arr){
             System.out.print(ele+ " ");
@@ -8,13 +8,13 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr={ 5 ,52 ,3 ,2 ,1};
         int n =arr.length;
-        for (int j = 0; j <= n-1; j++) {
-            for(int i=0;i<n-1;i++){
+        for (int j = 0; j < n-1; j++) {
+            for(int i=0;i<n-1-j;i++){
                 if(arr[i]>arr[i+1]){
                     int term = arr[i];
                     arr[i] =arr[i+1];
                     arr[i+1] =term;
-                    j--;
+                    
                 }
             }
             
--- Sorting/BubbleSort3.java
+++ Sorting/BubbleSort3.java
@@ -1,4 +1,4 @@
-public class BubbleSort {
+public class BubbleSort3 {
     public static void print(int[] arr){
         for(int ele : arr){
             System.out.print(ele+ " ");
@@ -6,17 +6,20 @@ public class BubbleSort {
         System.out.println();
     }    
     public static void main(String[] args) {
-        int[] arr={ 5 ,52 ,3 ,2 ,1};
+        int[] arr={ 5 ,52 ,3 ,10 ,1};
         int n =arr.length;
-        for (int j = 0; j <= n-1; j++) {
-            for(int i=0;i<n-1;i++){
+        for (int j = 0; j < n-1; j++) {
+            boolean flag = true;
+            for(int i=0;i<n-1-j;i++){
                 if(arr[i]>arr[i+1]){
                     int term = arr[i];
                     arr[i] =arr[i+1];
                     arr[i+1] =term;
-                    j--;
+                    flag = false;
+                    
                 }
             }
+            if(flag== true)break;
             
         }
         print(arr);

13bd0f29558d61287d2c9fddf4c1c25ec64f7ff2
mayankmalviya015 <mayankmalviya2620@gmail.com>
2026-02-14 15:40:07 +0530

    add sort folder

--- Sorting/Sort.java
+++ Sorting/Sort.java
@@ -1,5 +1,16 @@
 package Sorting;
 
 public class Sort {
-    
+    public static void main(String[] args) {
+        boolean x =true;
+        int[] arr={1, 3, 4, 5, 6, 7,8};
+        for(int i=0; i<arr.length -1; i++){
+            if(arr[i]>=arr[i+1])
+                x= false;
+        }
+        if(x==true)
+            System.out.println("sorted");
+        else System.err.println("unsort");
+
+    }
 }
